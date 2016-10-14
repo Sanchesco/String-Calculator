13ffa39a5654149faf98db9568513dda57698082
Sanchesco <magnust11@ru.is>
2016-10-14 23:54:04 +0000

    newline unittest

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -10,23 +10,35 @@ public class CalculatorTest {
     }
 
 	@Test
+	//testing for the empty string rule
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
 	@Test
+	//testing if we input the number 1, should return 1
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
 
 	@Test
+	//checkin the addition of two numbers
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
 	@Test
+	//checking the addition of multiple numbers
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+	@Test
+	//testing for the newline character
+	public void testNewLineSeperator() {
+		assertEquals(12, Calculator.add("1,2\n9"));
+	}
+
+	
+	
 }
