b091e8f0c8611c784f6d65d78ad24cbd0d1a350b
Sanchesco <magnust11@ru.is>
2016-10-15 00:03:50 +0000

    fixed main so that newline test passes

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,20 +6,23 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
+		else if (text.contains("//")) {
+			String[] split = splitNumbers(text,"\n");
+			String delim = split[0].substring(2);
+			return sum(splitNumbers(split[1], delim));
 		}
 		else
-			return 1;
+			return sum(splitNumbers(text, ",|\n"));
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
 
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	private static String[] splitNumbers(String numbers, String delim){
+	    return numbers.split(delim);
 	}
+	
       
     private static int sum(String[] numbers){
  	    int total = 0;
